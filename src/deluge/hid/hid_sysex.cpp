7e583915dd62def40373aff34463c4058a6d8dcb
bfredl <bjorn.linse@gmail.com>
2023-07-07 15:21:39 +0200

    send big sysex in big buffer

--- src/deluge/hid/hid_sysex.cpp
+++ src/deluge/hid/hid_sysex.cpp
@@ -39,6 +39,8 @@ void HIDSysex::requestOLEDDisplay(int ip, int d, int cable, uint8_t* data, int l
 			return;
 		}
 		sendOLEDData(ip, d, cable, row);
+	} else if (data[4] == 2 or data[4] == 3) {
+		sendOLEDDataAll(ip, d, cable, (data[4] == 3));
 	}
 
 }
@@ -52,6 +54,26 @@ void HIDSysex::sendOLEDData(int ip, int d, int cable, int blk) {
 
 	int packed = pack_8bit_to_7bit(reply+6, packed_block_size,
 								   &OLED::oledCurrentImage[blk][0], block_size);
+	if (packed != packed_block_size) {
+	  OLED::popupText("eror: fail");
+	}
+	reply[6+packed_block_size] = 0xf7; // end of transmission
+	midiEngine.sendSysex(ip, d, cable, reply, packed_block_size+7);
+}
+
+static uint8_t big_buffer[1024];
+
+void HIDSysex::sendOLEDDataAll(int ip, int d, int cable, bool rle) {
+	const int block_size = 768;
+	const int packed_block_size = 878;
+
+	uint8_t reply_hdr[5] = {0xf0, 0x7e, 0x02, 0x40, rle ? 0x02 : 0x01};
+	uint8_t *reply = big_buffer;
+	memcpy(reply, reply_hdr, 5);
+	reply[5] = 0;  // nominally 32*data[5] is start pos for a delta
+
+	int packed = pack_8bit_to_7bit(reply+6, packed_block_size,
+								   OLED::oledCurrentImage[0], block_size);
 	if (packed != packed_block_size) {
 	  OLED::popupText("eror: fail");
 	}
--- src/deluge/hid/hid_sysex.h
+++ src/deluge/hid/hid_sysex.h
@@ -4,5 +4,6 @@ namespace HIDSysex {
 	void requestOLEDDisplay(int ip, int d, int cable, uint8_t* data, int len);
 	void sysexReceived(int ip, int d, int cable, uint8_t* data, int len);
 	void sendOLEDData(int ip, int d, int cable, int blk);
+	void sendOLEDDataAll(int ip, int d, int cable, bool rle);
 
 }
